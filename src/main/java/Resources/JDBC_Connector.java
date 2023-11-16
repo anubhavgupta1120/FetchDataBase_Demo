04ae916a2db89c31d79b62a74b4b1ad8887979bc
anubhavgupta1120 <anubhavgp11@gmail.com>
2023-11-17 03:48:51 +0530

    new Changes in JDBC Connector class

--- src/main/java/Resources/JDBC_Connector.java
+++ src/main/java/Resources/JDBC_Connector.java
@@ -5,19 +5,34 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 
 public class JDBC_Connector {
 
 	public static Object[][] connect(String database) throws SQLException {
-		Object[][] data = new Object[1][2];
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+database+"", "root", "AnubhavGupta");
-		Statement statement = con.createStatement();
+		Object[][] data;
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database + "", "root",
+				"AnubhavGupta");
+		Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);// always
+																													// add
+																													// these
+																													// arguments
 		ResultSet resultSet = statement.executeQuery("select * from TestData;");
+		int colCount = resultSet.getMetaData().getColumnCount(); // To get the total count of coloumn
+		resultSet.last(); // To move to the last row
+		int rowCount = resultSet.getRow(); // get the index of last row so it will become eventually give the total no.
+											// of rows
+		resultSet.beforeFirst(); // again move back to first row
+		data = new Object[rowCount][colCount];
+		int i = 0;
 		while (resultSet.next()) {
-			data[0][0] = resultSet.getString("username");
-			data[0][1] = resultSet.getString("password");
+			for (int j = 0; j < colCount; j++) {
+				data[i][j] = resultSet.getString(j + 1);
+			}
+			i++;
 		}
-		
+		con.close();
+		System.out.println(Arrays.deepToString(data));
 		return data;
 
 	}
--- src/test/java/TestFolders/GetDataFromDB.java
+++ src/test/java/TestFolders/GetDataFromDB.java
@@ -1,43 +1,44 @@
 package TestFolders;
 
-import org.testng.annotations.Test;
-
-import PageObject.LoginPage;
-import Resources.JDBC_Connector;
-
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-
 import java.sql.SQLException;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import PageObject.LoginPage;
+import Resources.JDBC_Connector;
 
 public class GetDataFromDB {
 	WebDriver driver;
-  @Test(dataProvider = "getData")
-  public void Test(String username, String password) {
-	  LoginPage loginPage = new LoginPage(driver);
-	  loginPage.loginIntoApplication(username, password);
-  }
-  @BeforeTest
-  public void beforeTest() {
-	  driver = new ChromeDriver();
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	  driver.manage().window().maximize();
-	  driver.get("https://crio-qkart-frontend-qa.vercel.app/login");
-  }
 
-  @AfterTest
-  public void afterTest() {
-	  driver.quit();
-  }
-  
-  @DataProvider
-  public Object[][] getData() throws SQLException {
-	 return JDBC_Connector.connect("TDQKart");
-  }
+	@Test(dataProvider = "getData")
+	public void Test(String username, String password) throws InterruptedException {
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.loginIntoApplication(username, password);
+//		Thread.sleep(5000);
+	}
+
+	@BeforeMethod(alwaysRun = true)
+	public void beforeTest() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://crio-qkart-frontend-qa.vercel.app/login");
+	}
+
+	@AfterMethod(alwaysRun = true)
+	public void afterTest() {
+		driver.quit();
+	}
+
+	@DataProvider
+	public Object[][] getData() throws SQLException {
+		return JDBC_Connector.connect("TDQKart");
+	}
 
 }
